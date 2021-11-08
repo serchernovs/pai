f1af0636ffb96adb39fb3643d6e3734a6ee441de
Sergey <sergeichernov.chernov@yandex.ru>
2021-11-09 01:05:55 +0500

    proverca

--- bot.py
+++ bot.py
@@ -2,10 +2,14 @@ from telegram.ext import Updater, CommandHandler
 
 print("бот запущен")
 
-token="2142334978:AAFpJpjcjIBPlIO7WMJYHbd9gr1woKDJopw"
+def on_start(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text="Привет, chernov1993 bot")
+    
+token ="2142334978:AAFpJpjcjIBPlIO7WMJYHbd9gr1woKDJopw"
 updater = Updater(token, use_context=True)
+dispatcher.add_handler(CommandHandler("start", on_start))
 
-updater.start_
 
 updater.start_polling()
 updater.idle()
