99d5f822a31333a0c4a29d01c8c569c6c213b088
Sergey <sergeichernov.chernov@yandex.ru>
2021-11-10 21:02:36 +0500

    valut bot

--- bot.py
+++ bot.py
@@ -4,9 +4,20 @@ print("бот запущен")
 
 def on_start(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text="Привет, chernov1993 bot")
+    context.bot.send_message(chat_id=chat.id, text="Привет, sergei1993valutnii_bot")
     
-token ="2142334978:AAFpJpjcjIBPlIO7WMJYHbd9gr1woKDJopw"
+def on_message(update, context):
+	chat = update.effective_chat
+	text = update.message.text
+	try:
+		number = float(text)
+		rate = 80.34
+		soms = number * rate
+		message = "$%.2f = %.2f сом" % (number, soms)
+		context.bot.send_message(chat_id=chat.id, text=message)
+	except:
+    
+token ="2124393369:AAEfbl-lPNuvrblZAqi0Fox9dav-kfLSrCY"
 dispatcher.add_handler(MessageHandler(Filters.all, on_message))
 updater = Updater(token, use_context=True)
 dispatcher = updater.dispatcher
